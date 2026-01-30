fd6b3e8ef27718a5535fea4d9b07c3152feb6d9b
Salima Kherchache <salima.kherchache@inrae.fr>
2026-01-30 11:54:21 +0100

    fix: optimisation manifest pour iOS et Android

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,23 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      // Utilisation de @ pour pointer vers la racine du projet
+      '@': path.resolve(__dirname, './src'), 
+    }
+  },
+  build: {
+    // Optimisation pour la production
+    sourcemap: false,
+    chunkSizeWarningLimit: 1600,
+  }
 });
